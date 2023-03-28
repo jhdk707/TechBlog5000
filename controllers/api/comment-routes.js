93bf97c09248399c4282f2738c2f491155553f11
Doomtower <jessehudak707@outlook.com>
2023-03-28 07:44:52 -0700

    working on heroku bugs and comment posts

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const { Comment } = require("../../models");
 const withAuth = require("../../utils/auth");
+
 router.get("/", (req, res) => {
   Comment.findAll({})
     .then((dbCommentData) => res.json(dbCommentData))
@@ -24,13 +25,17 @@ router.get("/:id", (req, res) => {
 });
 
 router.post("/", withAuth, (req, res) => {
+  console.log(req.body); // Log the request body to see what data you're receiving
   if (req.session) {
     Comment.create({
       comment_text: req.body.comment_text,
       post_id: req.body.post_id,
       user_id: req.session.user_id,
     })
-      .then((dbCommentData) => res.json(dbCommentData))
+      .then((dbCommentData) => {
+        console.log(dbCommentData); // Log the newly created comment data to see if it was created successfully
+        res.json(dbCommentData);
+      })
       .catch((err) => {
         console.log(err);
         res.status(400).json(err);
@@ -80,4 +85,5 @@ router.delete("/:id", withAuth, (req, res) => {
       res.status(500).json(err);
     });
 });
+
 module.exports = router;
--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -3,17 +3,14 @@ const { User } = require("../models");
 const userData = [
   {
     username: "Jesse",
-    email: "jesse@test.com",
     password: "test369mcv",
   },
   {
     username: "Jenn707",
-    email: "jenn@salesteam.com",
     password: "876gonuts",
   },
   {
     username: "Larrietta",
-    email: "ljones99@foobar.com",
     password: "ferrarigofast",
   },
 ];
